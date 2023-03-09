d111ef4c73c03624c7b6b4cb45a628c87cab2d42
Cory Ritz <coryritz@bellsouth.net>
2023-03-09 10:57:31 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const cookie = document.getElementById('cookie');
+const counter = document.getElementById('counter');
+
+
+ document.addEventListener('click', () => {
+    counter.innerText = parseInt(counter.innerText) + 1
+})
